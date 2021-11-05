2110bcf44a33aadcc4e3d506b1de0e751ff83dde
Stackbit Code Editor <projects@stackbit.com>
2021-11-05 19:35:46 +0000

    _document.js: changed

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -35,7 +35,7 @@ export default class MyDocument extends Document {
                 <!-- Global site tag (gtag.js) - Google Analytics -->
                 <script async src="https://www.googletagmanager.com/gtag/js?id=G-WLCGE78DDX"></script>
                 <script>
-  window.dataLayer = window.dataLayer || [];
+                window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
